f46a4514ebd3d84e2ccaf44a6f2da93309942d9c
work_6 <saramet.constantin-dorin@nxp.com>
2020-02-26 17:34:37 +0200

    GUI/MediaPlayer/
    - Update

--- GUI/MediaPlayer/main.py
+++ GUI/MediaPlayer/main.py
@@ -3,6 +3,9 @@ from pygame import mixer
 from tkinter import filedialog
 from tkinter import messagebox
 
+"""
+    https://www.youtube.com/playlist?list=PLhTjy8cBISEp6lNKUO3iwbB1DKAkRwutl
+"""
 root = tk.Tk()
 
 mixer.init()
@@ -29,11 +32,15 @@ def play():
     play_file('media/song1.mp3')
     statusbar['text'] = "Music play"
 
+def stop():
+    mixer.music.stop()
+    statusbar['text'] = "Music stoped"
+
 def pause():
     global paused
     paused = True
     mixer.music.pause()
-    statusbar['text'] = "Music stoped"
+    statusbar['text'] = "Music paused"
 
 def set_media_volume(val):
     volume = int(val) / 100
@@ -46,7 +53,6 @@ def browse_file():
 
 root.title('Media player')
 root.iconbitmap('img/app.ico')
-root.geometry('600x400')
 
 menubar = tk.Menu(root)
 root.config(menu=menubar)
@@ -58,16 +64,21 @@ menu_file.add_command(label="Exit", command=root.destroy)
 menubar.add_cascade(label='File', menu=menu_file)
 
 # --------------------------------------------------------------------------
-img_piano = tk.PhotoImage(file='img/piano.png')
-lbl_msg = tk.Label(root, text='Welcome to Media player')
-lbl_img_piano = tk.Label(root, image=img_piano)
+
+frame_buttons = tk.Frame(root)
+
+# --------------------------------------------------------------------------
+lbl_msg = tk.Label(root, text='Music is life')
 
 # --------------------------------------------------------------------------
 img_play = tk.PhotoImage(file='img/play.png')
-btn_play = tk.Button(root, text='', image=img_play, command=play)
+btn_play = tk.Button(frame_buttons, text='', image=img_play, command=play)
+
+img_stop = tk.PhotoImage(file='img/stop.png')
+btn_stop = tk.Button(frame_buttons, text='', image=img_stop, command=stop)
 
 img_pause = tk.PhotoImage(file='img/pause.png')
-btn_pause = tk.Button(root, text='', image=img_pause, command=pause)
+btn_pause = tk.Button(frame_buttons, text='', image=img_pause, command=pause)
 
 # --------------------------------------------------------------------------
 scale_volum = tk.Scale(root, from_=0, to=100, orient=tk.HORIZONTAL, command=set_media_volume)
@@ -77,11 +88,13 @@ scale_volum.set(15)
 statusbar = tk.Label(root, text='Welcom to Media player', bg='yellow')
 
 # --------------------------------------------------------------------------
-lbl_msg.grid(row=0, column=0, columnspan=10)
-lbl_img_piano.grid(row=1, column=0, columnspan=10)
-btn_play.grid(row=2, column=0)
-btn_pause.grid(row=2, column=3)
-scale_volum.grid(row=3, column=0, columnspan=3)
+btn_play.grid(row=2, column=0, padx=5)
+btn_stop.grid(row=2, column=2, padx=5)
+btn_pause.grid(row=2, column=4, padx=5)
+
+lbl_msg.grid(row=0, column=0, columnspan=10, pady=15)
+frame_buttons.grid(row=2, column=0, columnspan=8, padx=15)
+scale_volum.grid(row=3, column=0, columnspan=10, pady=10)
 statusbar.grid(row=99, column=0, columnspan=10, sticky=tk.S)
 
 root.mainloop()
